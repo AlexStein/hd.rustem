0b2970dc514173aa7c3a2db79b27da679a2bbb72
Alex Shevelev <alex@softcomplex.ru>
2017-12-20 12:46:58 +0300

    Fix 4cron archive task

--- sys/4cron.php
+++ sys/4cron.php
@@ -1,14 +1,13 @@
 <?php
-$base = dirname(dirname(__FILE__)); 
+$base = dirname(dirname(__FILE__));
 include($base ."/conf.php");
 date_default_timezone_set('Europe/Kiev');
 function humanTiming_old ($time)
 {
-
     $time = time() - $time;
-
     return floor($time/86400);
 }
+
 $dbConnection = new PDO(
     'mysql:host='.$CONF_DB['host'].';dbname='.$CONF_DB['db_name'],
     $CONF_DB['username'],
@@ -19,54 +18,37 @@ $dbConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 function get_conf_param($in) {
- global $dbConnection;
- $stmt = $dbConnection->prepare('SELECT value FROM perf where param=:in');
- $stmt->execute(array(':in' => $in));
- $con = $stmt->fetch(PDO::FETCH_ASSOC);
+    global $dbConnection;
+    $stmt = $dbConnection->prepare('SELECT value FROM perf where param=:in');
+    $stmt->execute(array(':in' => $in));
+    $con = $stmt->fetch(PDO::FETCH_ASSOC);
 
-return $con['value'];
-
+    return $con['value'];
 }
 
-$CONF = array (
-'days2arch'   => get_conf_param('days2arch')
-);
+$CONF = array ('days2arch' => get_conf_param('days2arch'));
 
-
-
-
-
-/*   
+/*
 5 0 * * * /usr/bin/php5 -f /var/www/hd_prod/sys/4cron.php > /var/www/hd_prod/4cron.log 2>&1
 */
 
-            $stmt = $dbConnection->prepare('SELECT id, ok_by, ok_date,date_create
-							from tickets
-							where arch=:n1 and ok_by !=:n2');
-			$stmt->execute(array(':n1' => '0',':n2' => '0'));
-			$res1 = $stmt->fetchAll();
+$stmt = $dbConnection->prepare('SELECT id, ok_by, ok_date, date_create
+							    FROM tickets
+							    WHERE arch=:n1 AND ok_by !=:n2');
+$stmt->execute(array(':n1' => '0',':n2' => '0'));
+$res1 = $stmt->fetchAll();
 foreach($res1 as $row) {
 
-
-
-
     $m=$row['id'];
-    $td= humanTiming_old(strtotime($row['ok_date']))."<br>";
+    $td=humanTiming_old(strtotime($row['ok_date']));
 
     if ($td > $CONF['days2arch'] ) {
 
-                $stmt = $dbConnection->prepare('update tickets set arch=:n1, last_update=now() where id=:m');
+        $stmt = $dbConnection->prepare('UPDATE tickets SET arch=:n1, last_update=now() WHERE id=:m');
 		$stmt->execute(array(':n1' => '1',':m' => $m));
-        
-        
-        
-                
-                
-            $stmt = $dbConnection->prepare('INSERT INTO ticket_log (msg, date_op, ticket_id)
+        $stmt = $dbConnection->prepare('INSERT INTO ticket_log (msg, date_op, ticket_id)
 values (:arch, now(), :m)');
-			$stmt->execute(array(':arch' => 'arch',':m' => $m));
+        $stmt->execute(array(':arch' => 'arch',':m' => $m));
     }
-
 }
-
 ?>
